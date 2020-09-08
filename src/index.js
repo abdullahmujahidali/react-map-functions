d6018609cb8268538292a2b7b3ffdfa4031c3dc5
Abdullah Mujahid <48271080+abdullahmujahidali@users.noreply.github.com>
2020-09-08 09:55:26 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
 var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
-function double(x) {
-  return x * 2;
-}
+// function double(x) {
+//   return x * 2;
+// }
 
-const newArray = numbers.map(double);
+// const newArray = numbers.map(double);
 
-console.log(newArray);
+// console.log(newArray);
 
 //Filter - Create a new array by keeping the items that return true.
 
-function filterReq(x) {
-  if (x > 10) {
-    return x;
-  }
-}
-const newNumbers = numbers.filter(filterReq);
-console.log(newNumbers);
+// function filterReq(x) {
+//   if (x > 10) {
+//     return x;
+//   }
+// }
+// const newNumbers = numbers.filter(filterReq);
+// console.log(newNumbers);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
+
+var newReduce= numbers.reduce(function(accum, current){
+  return accum+current;
+})
+
+console.log(newReduce);
+
 
 //Find - find the first item that matches from an array.
 
